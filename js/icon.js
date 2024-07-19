f3882af06c7827c0534b51b49f53169d580a72dc
Lewis <lewis115@marshall.edu>
2024-07-19 17:04:02 -0400

    icons & data

---
+++ js/icon.js
@@ -0,0 +1,52 @@
+// Define custom icons
+var forestIcon = L.icon({
+    iconUrl: './images/icons/forest.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+var restaurantIcon = L.icon({
+    iconUrl: './images/icons/restaurant.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+var retailIcon = L.icon({
+    iconUrl: './images/icons/supermarket.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+
+var damIcon = L.icon({
+    iconUrl: './images/icons/dam_red.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+var fillingstationIcon = L.icon({
+    iconUrl: './images/icons/fillingstation_black.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+var farmIcon  = L.icon({
+    iconUrl: './images/icons/cow-export_green.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+var golfIcon  = L.icon({
+    iconUrl: './images/icons/golfing_green.png', // URL to your custom icon image
+    iconSize: [32, 37], // Size of the icon
+    iconAnchor: [22, 38], // Point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -38] // Point from which the popup should open relative to the iconAnchor
+});
+
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const riverAccess = 'data/Twelvepole_Access.geojson'
+const places = 'data/Twelvepole_Places.geojson'
+
+
 // Initialize the map
 var map = L.map('map').setView([38.28023543166722, -82.45273397151891], 13);
 
@@ -6,9 +10,67 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors',
 }).addTo(map);
 
-fetch('data/accessPoints.geojson')
-  .then((response) => response.json())
-  .then((data) => {
-    L.geoJSON(data).addTo(map);
-  })
-  .catch((error) => console.error('Error loading GeoJSON:', error));
+// addGeoJson(riverAccess)
+addGeoJson(places)
+
+// Function to create a circle marker
+function pointToLayer(feature, latlng) {
+  return L.circleMarker(latlng, style(feature));
+}
+
+// Function to determine color based on feature properties
+function getIcon(type) {
+  console.log(retailIcon)
+  switch (type) {
+    case 'Boat Dealer':
+    case 'Dollar Store':
+    case 'Retail Store':
+      return retailIcon; // Red for type1
+    case 'Dam':
+      return damIcon; // Red for type1
+    case 'Restaurant':
+    case 'Cafe':
+      return restaurantIcon; // Blue for type2
+    case 'Gas Station':
+      return fillingstationIcon;
+    case 'Recreation/Golf':
+      return golfIcon;
+    case 'Recreation/Ball Fields':
+    case 'Recreation/Playgrounds':
+      return forestIcon;
+    case 'Farm':
+      return farmIcon;
+    default: return '#00FF00'; // Green for others
+  }
+}
+
+// Function to style each feature
+// function style(feature) {
+//   return {
+//     radius: 8,
+//     fillColor: getColor(feature.properties.Type),
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.8
+//   };
+// }
+
+function addGeoJson(data) {
+  fetch(data)
+    .then((response) => response.json())
+    .then((data) => {
+      L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+          return L.marker(latlng, { icon: getIcon(feature.properties.Type) });
+      }
+      }).addTo(map);
+    })
+    .catch((error) => console.error('Error loading GeoJSON:', error));
+}
+// fetch(riverAccess)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     L.geoJSON(data).addTo(map);
+//   })
+//   .catch((error) => console.error('Error loading GeoJSON:', error));
